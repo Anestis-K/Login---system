1e9ad01c0749ca96e3fa50c1d9e5f8f6b108a5c9
Anestis Karonidis <anestis.karonidis@gmail.com>
2020-07-24 09:23:41 +0300

    Add files via upload

---
+++ Beginner.py
@@ -0,0 +1,38 @@
+
+
+users = {}  # what is {}
+status = ""  # what is status ""
+
+
+def displaymenu():  # what is a function
+    status = input('are you a registerd user?')
+    if status == 'y':
+        oldUser()
+    elif status == 'n':
+        newUser()
+
+
+def newUser():
+    createlogin = input('create your codex')
+
+    if createlogin in users:
+        print('login already exists')
+    else:
+        createpassw = input('create password')
+        users[createlogin] = createpassw
+        print('User created')
+
+
+def oldUser():
+    login = input('Enter login name')
+    passw = input('Enter the password')
+
+    if login in users and users[login] == passw:
+        print('login successful')
+    else:
+        print('user does not exist or wrong password')
+
+
+while status != 'q':
+    displaymenu()
+
